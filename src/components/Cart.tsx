e147e1a543e0ca531a54ae73589cb74715a4fef6
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-22 19:29:29 +1300

    added content to cart - still needs dynamic functionality

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,18 @@
 "use-client"
 
-import { ShoppingCart } from "lucide-react"
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Divide, ShoppingCart } from "lucide-react"
+import { 
+  Sheet, 
+  SheetContent, 
+  SheetHeader, 
+  SheetTitle, 
+  SheetTrigger 
+} from "./ui/sheet"
+import { Separator } from './ui/separator'
 
 const Cart = () => {
+
+  const itemCount = 1
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -14,15 +23,38 @@ const Cart = () => {
         <span 
           className="ml-2 text-sm font-medium text-graay-700 group-hover:text-gray-800"
         >
-          0
+          1
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>
-            Cart (0)
+            Cart (1)
           </SheetTitle>
         </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* TODO: Cart Logic */}
+              Cart Items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>$1</span>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
       </SheetContent>
     </Sheet>
   )
