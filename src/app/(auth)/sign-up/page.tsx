79cc9e800bd1d921a5cfee4df5c088678f2b9b38
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-24 20:05:21 +1300

    updated sign up process

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -30,13 +30,13 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   })
 
-  const { data } = trpc.anyApiRoute.useQuery()
+  const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({})
 
   const onSubmit = ({
     email, 
     password,
   }: TAuthCredentialsValidator) => {
-    // send data to the server
+    mutate({ email, password })
 
   }
 
@@ -82,6 +82,7 @@ const Page = () => {
                   <Label htmlFor="password">Password</Label>
                   <Input 
                     {...register("password")}
+                    type="password"
                     className={cn({
                       "focus-visible:ring-red-500": 
                       errors.password,
