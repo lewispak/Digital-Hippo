f0d09a9e472e47adc61e0aaf1d3757d4d488bcc9
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-24 19:22:27 +1300

    expanded auth flow and fixed a couple bugs

---
+++ src/collections/Users.ts
@@ -0,0 +1,27 @@
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+  slug: "users",
+  auth: true,
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "role",
+      type: "select",
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "User",
+          value: "user",
+        }
+      ]
+    }
+  ]
+
+}
