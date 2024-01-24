da48b0b9309d037634d1ee41d1d486c2501abbf7
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-24 20:54:35 +1300

    setup verification email with resend. Need own domain to receive verification email

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,13 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML:({token}) => {
+        return `<p>Please verify your email account.</p>`
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
