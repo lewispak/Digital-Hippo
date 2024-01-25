d90c8392d3a2f3663b33769d705096a57ce0db88
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-25 20:50:42 +1300

    added sign in feature

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -56,4 +56,30 @@ export const authRouter = router({
 
         return { success: true }
   }),
+
+  //--- Sign In User API Route ---//
+  signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+    const { email, password } = input 
+    const { res } = ctx
+
+
+    const payload = await getPayloadClient()
+
+    try {
+      await payload.login({
+        collection: "users",
+        data: {
+          email,
+          password
+        },
+        res,
+      })
+
+      return { success: true }
+    } catch (err) {
+      throw new TRPCError({ code: "UNAUTHORIZED" })
+    }
+  })
 })
