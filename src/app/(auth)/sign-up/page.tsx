7d0089e2499d9b15319ff1832633045e193cb436
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-25 19:26:13 +1300

    added redirection to check email page after sign up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -19,6 +19,8 @@ import {
 } from "@/lib/validators/account-credentials-validator"
 import { trpc } from '@/trpc/client'
 import { toast } from "sonner"
+import { ZodError } from "zod"
+import { useRouter } from "next/navigation"
 
 
 const Page = () => {
@@ -31,12 +33,32 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   })
 
+  const router = useRouter()
+
   const {mutate, isLoading} = 
     trpc.auth.createPayloadUser.useMutation({
       onError: (err) => {
         if(err.data?.code === "CONFLICT") {
-          toast.error("This email is already in use. Sign in instead?")
+          toast.error(
+            "This email is already in use. Sign in instead?"
+          )
+
+          return
         }
+
+        if(err instanceof ZodError) {
+          toast.error(err.issues[0].message)
+
+          return 
+        }
+
+        toast.error(
+          "Something went wrong. Please try again."
+        )
+      },
+      onSuccess: ({sentToEmail}) => {
+        toast.success(`Verification email sent to ${sentToEmail}.`)
+        router.push("/verify-email?to=" + sentToEmail)
       }
     })
 
