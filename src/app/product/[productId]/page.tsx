f183f86db5a436d71757ea5ac705dcaa4e25d5ad
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-29 16:50:46 +1300

    added product category and price to product detail page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,5 +1,9 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import { PRODUCT_CATEGORIES } from "@/config"
+import { getPayloadClient } from "@/get-payload"
+import { formatPrice } from "@/lib/utils"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface PageProps {
   params: {
@@ -20,12 +24,38 @@ const BREADCRUMBS = [
   },
 ]
 
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
+  const { productId } = params
+  const payload = await getPayloadClient()
+
+  const {docs: products} = await payload.find({
+    collection: "products",
+    limit: 1,
+    where: {
+      id: {
+        equals: productId
+      },
+      approvedForSale: {
+        equals: "approved",
+      },
+    },
+  })
+
+  const [product] = products
+
+  if (!products) return notFound()
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({value}) => value === product.category
+    )?.label
+
   return (
     <MaxWidthWrapper className="bg-white">
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           {/* --- Product Details --- */}
+
+          {/* --- Breadcrumb --- */}
           <div className="lg:mx-w-lg lg:self-end ">
             <ol className="flex flex-center space-x-2 ">
               {BREADCRUMBS.map((breadcrumb, i) => (
@@ -51,6 +81,24 @@ const Page = ({ params }: PageProps) => {
                 </li>
               ))}
             </ol>
+
+            <div className="mt-4">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {product.name}
+              </h1>
+            </div>
+
+            <section className="mt-4">
+              <div className="flex items-center">
+                <p className="font-medium text-gray-900">
+                  {formatPrice(product.price)}
+                </p>
+
+                <div className="ml-4 border-l text-muted-foreground border-gray-300 pl-4">
+                  {label}
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
