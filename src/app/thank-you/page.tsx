5962858f962f65583607d37b1f1a5ec761295398
Lewis Pakoti <67088984+lewispak@users.noreply.github.com>
2024-01-30 23:01:42 +1300

    finished styling thank you page

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,8 +3,11 @@ import Image from "next/image"
 import { cookies } from "next/headers"
 import { getPayloadClient } from "@/get-payload"
 import { notFound, redirect } from "next/navigation"
-import { Product, ProductFile } from "@/payload-types"
+import { Product, ProductFile, User } from "@/payload-types"
 import { PRODUCT_CATEGORIES } from '@/config'
+import { formatPrice } from "@/lib/utils"
+import Link from "next/link"
+import PaymentStatus from "@/components/PaymentStatus"
 
 
 interface PageProps {
@@ -42,6 +45,12 @@ const ThankYouPage = async ({searchParams}: PageProps) => {
   if(orderUserId !== user?.id) {
     return redirect(`/sign-in?origin=thank-you?orderId=${order.id}`)
   }
+
+  const products = order.products as Product[]
+
+  const orderTotal = products.reduce((total, product) => {
+    return total + product.price
+  }, 0)
 
   return (
     <main className="relative lg:min-h-full">
@@ -127,19 +136,56 @@ const ThankYouPage = async ({searchParams}: PageProps) => {
                             </a>
                           ) : null}
                         </div>
+
+                        <div className="flex-none font-medium text-gray-900">
+                          {formatPrice(product.price)}
+
+                        </div>
                       </li>
                     )
                   })}
                 </ul>
 
+                <div className="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground">
+                  <div className="flex justify-between">
+                    <p>Subtotal</p>
+                    <p className="text-gray-900">{formatPrice(orderTotal)}</p>
+                  </div>
+
+                  <div className="flex justify-between">
+                    <p>Transaction Fee</p>
+                    <p className="text-gray-900">{formatPrice(1)}</p>
+                  </div>
+
+                  <div className="flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900">
+                    <p className="text-base">
+                      Total
+                    </p>
+                    <p className="text-base">{formatPrice(orderTotal + 1)}</p>
+                  </div>
+                </div>
+
+                <PaymentStatus 
+                  isPaid={order._isPaid} 
+                  orderEmail={(order.user as User).email}
+                  orderId={order.id}
+                />
+
+                <div className="mt-16 border-t border-gray-200 py-6 text-right">
+                  <Link 
+                    href="/products" 
+                    className="text-sm font-medium text-blue-600 hover:text-blue-500"
+                  >
+                    Continue Shopping &rarr;
+                  </Link>
+
+                </div>
               </div>
           </div>
         </div>
       </div>
-
     </main>
   )
-
 }
 
 export default ThankYouPage
